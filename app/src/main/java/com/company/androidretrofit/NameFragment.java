c11a73a55b7ef50b0f226060704c5401e782f5f6
anastasiaShpakava <shpakavaa@gmail.com>
2020-10-29 17:05:46 +0300

    added logic for menu fragments

--- app/src/main/java/com/company/androidretrofit/NameFragment.java
+++ app/src/main/java/com/company/androidretrofit/NameFragment.java
@@ -3,17 +3,12 @@ package com.company.androidretrofit;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
